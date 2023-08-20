6d2fb323d3fed0a5e5eeaaa4b563bffc7e7d9794
Filipe Francisco <111152966+filipe-f-franknberger@users.noreply.github.com>
2023-08-20 19:19:50 -0300

    Add files via upload

--- EstruturaDeDadosII/src/BinaryTree/Main.java
+++ EstruturaDeDadosII/src/BinaryTree/Main.java
@@ -1,20 +1,22 @@
 package BinaryTree;
 
+import ListaEncadeada.ListaEncadeada;
+
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+				
 		SearchTree tree = new SearchTree();		
-		tree.insert(25);
-		tree.insert(22);
-		tree.insert(35);
-		tree.insert(6666);
-		tree.insert(21);
-		tree.insert(22);
+		tree.insert(10);
+		tree.insert(20);
+		tree.insert(13);
+		tree.insert(40);
+		tree.insert(3);
+		tree.insert(1232);
+		tree.insert(2734);
 		tree.insert(27);
-		tree.insert(27434);
-		tree.insert(273);
+		tree.insert(20000);
 		
 		System.out.println("Before removing:");
 		
@@ -31,9 +33,55 @@ public class Main {
 		
 		tree.showInLevelingOrder();
 		
+		//a) Mostrar o maior número
 		tree.showBiggestNumber();
 		
+		//b) Mostrar o menor número
 		tree.showSmallestNumber();
+
+		//c) Mostrar os nós folhas
+		System.out.println("Nós folhas:");
+		tree.mostrarFolha();
+		
+		//d) Mostrar os nós ancestrais de um nó
+		System.out.println("Ancestrais: ");
+		tree.verAncestral(40);
+		
+		//e) Mostrar os nós descendentes de um nó
+		System.out.println("\nDescendentes: ");
+		tree.verDescendentes(40);
+		
+		//f) Mostrar a subárvore da direita de um nó
+		System.out.println("\nSubarvore da direita: ");
+		tree.verSubDireita(10);
+		
+		//g) Mostrar a subárvore da esquerda de um nó
+		System.out.println("\nSubarvore da esquerda: ");
+		tree.verSubEsquerda(10);
+		
+		//h) Transformar a árvore numa lista encadeada
+		System.out.println("Árvore convertida em lista encadeada: ");
+		ListaEncadeada lista = new ListaEncadeada();
+		lista = tree.converterParaListaEncadeada();
+		lista.mostraLista();
+
+		//i) Mostrar somente os números pares
+		System.out.println("Pares:");
+		tree.showJustPairs();
+		
+		//j) Mostrar o nível de um nodo
+		int nivel = tree.verNivelDoNodo(40);
+		System.out.println("Nível do nodo 40 : "+nivel);
+		
+		//k) Mostrar a altura da árvore
+		int altura = tree.calcularAlturaArvore();
+		System.out.println("Altura da árvore: "+altura);
+		
+		//l) Mostra o tamanho da árvore
+		System.out.println("Tamanho da árvore: "+tree.getTamanho_arvore());
+		
+		//m) Inserir um novo item na árvore, mas de forma não recursiva	
+		
 		
 	}
 }
--- EstruturaDeDadosII/src/BinaryTree/SearchTree.java
+++ EstruturaDeDadosII/src/BinaryTree/SearchTree.java
@@ -1,9 +1,16 @@
 package BinaryTree;
 
 import java.util.Queue;
+
+import ListaEncadeada.ListaEncadeada;
+
 import java.util.LinkedList;
 
 public class SearchTree {
+	private int tamanho_arvore;
+	private int altura_arvore;
+	private ListaEncadeada lista_encadeada;
+    
 	private class Nodo{
 		private int key;
 		private Nodo right,left;
@@ -14,6 +21,26 @@ public class SearchTree {
 		}
 	}
 	
+	public SearchTree() {
+		lista_encadeada = new ListaEncadeada();
+	}
+
+	public int getTamanho_arvore() {
+		return tamanho_arvore;
+	}
+
+	public void setTamanho_arvore(int tamanho_arvore) {
+		this.tamanho_arvore = tamanho_arvore;
+	}
+
+	public int getAltura_arvore() {
+		return altura_arvore;
+	}
+
+	public void setAltura_arvore(int altura_arvore) {
+		this.altura_arvore = altura_arvore;
+	}
+
 	//a
 	public void showBiggestNumber() {
 		showBiggest(raiz);
@@ -84,7 +111,8 @@ public class SearchTree {
 		if(raiz == null) {
 			raiz = new Nodo(key);
 			System.out.println("inserted at the root");
-				return raiz;
+			tamanho_arvore++;
+			return raiz;
 		}
 		if(key < raiz.key) {
 			raiz.left = insertData(raiz.left,key);
@@ -98,6 +126,20 @@ public class SearchTree {
 		
 		return raiz;
 	}
+	
+	public void showJustPairs() {
+		showPairs(raiz);
+	}
+	
+	private void showPairs(Nodo raiz) {
+		if(raiz != null) {
+			showPairs(raiz.left);
+		
+			if(raiz.key % 2 == 0)System.out.println(raiz.key);
+			showPairs(raiz.right);
+		}
+	}
+	
 	
 	public void showInOrder() {
 		showOrderly(raiz);
@@ -164,8 +206,10 @@ public class SearchTree {
 			raiz.right = removeItem(raiz.right, chave);
 		}else {
 			if(raiz.left == null) {
+				tamanho_arvore--;
 				return raiz.right;
 			}else if(raiz.right == null) {
+				tamanho_arvore--;
 				return raiz.left;
 			}else {
 				Nodo next = findNext(raiz.right);
@@ -173,7 +217,6 @@ public class SearchTree {
 				raiz.right = removeItem(raiz.right,next.key);
 			}
 		}
-		
 	return raiz;
 	}
 	
@@ -184,4 +227,169 @@ public class SearchTree {
 		return nodo;
 	}
 	
+	public int calcularAlturaArvore() {
+	    return calcularAltura(raiz);
+	}
+
+	private int calcularAltura(Nodo raiz) {
+	    if (raiz == null) {
+	        return 0; 
+	    }
+
+	    int alturaEsquerda = calcularAltura(raiz.left);
+	    int alturaDireita = calcularAltura(raiz.right);
+
+	    if(alturaEsquerda > alturaDireita) {
+	    	return(alturaEsquerda+1);
+	    }else {
+	    	return(alturaDireita+1);
+	    }
+	    
+	}
+
+	public ListaEncadeada converterParaListaEncadeada() {
+      	  converterLista(raiz);
+      	  return lista_encadeada;
+	 }
+
+	private void converterLista(Nodo raiz) {
+		if (raiz == null) {
+			return;
+		}
+	    converterLista(raiz.left);
+	    lista_encadeada.inserirOrdenado(raiz.key);       
+
+	    converterLista(raiz.right);
+	}
+	
+	 public int verNivelDoNodo(int numero) {
+	        return verNivel(raiz, numero, 0);
+	 }
+
+	 private int verNivel(Nodo nodo, int numero, int nivel) {
+	        if (nodo == null) {
+	            return -1;
+	        }
+	        if (nodo.key == numero) {
+	            return nivel; 
+	        }
+
+	        int nivelEsquerda = verNivel(nodo.left, numero, nivel + 1);
+	        if (nivelEsquerda != -1) {
+	            return nivelEsquerda;
+	        }
+	        return verNivel(nodo.right, numero, nivel + 1);		       
+	 
+	 }
+	 
+	 public void mostrarFolha() {
+		 mostrarFolhas(raiz);
+	 }
+	 
+	 private void mostrarFolhas(Nodo raiz) {
+			if(raiz != null) {
+				
+				if(raiz.left == null  && raiz.right == null) {
+					System.out.println(raiz.key);
+				}
+				
+				mostrarFolhas(raiz.right);			
+				mostrarFolhas(raiz.left);
+			}
+	}
+	 
+	public void verSubDireita(int valor) {
+		
+		verSubDir(raiz, valor,false);
+	}
+	
+	private void verSubDir(Nodo raiz, int valor, boolean a) {
+		if(raiz != null) {
+			if(raiz.key == valor){
+				a = true;
+			}
+			if(a) {
+				System.out.println(raiz.key);
+			}
+			
+			verSubDir(raiz.right,valor,a);
+		}			
+		
+	}
+	
+	public void verSubEsquerda(int valor) {
+		
+		verSubEsq(raiz, valor,false);
+	}
+	
+	private void verSubEsq(Nodo raiz, int valor, boolean a) {
+		if(raiz != null) {
+			if(raiz.key == valor){
+				a = true;
+			}
+			if(a) {
+				System.out.println(raiz.key);
+			}
+			
+			verSubEsq(raiz.left,valor,a);
+		}			
+		
+	}
+	
+	public void verAncestral(int valorAlvo) {
+        verAncestralNo(raiz, valorAlvo);
+    }
+
+    private boolean verAncestralNo(Nodo nodo, int valorAlvo) {
+        if (nodo == null) {
+            return false;
+        }
+
+        if (nodo.key == valorAlvo) {
+            return true; 
+        }
+
+        if (verAncestralNo(nodo.left, valorAlvo) || verAncestralNo(nodo.right, valorAlvo)) {
+            System.out.print(nodo.key + " ");
+            return true;
+        }
+
+        return false;
+    }
+    
+    public void verDescendentes(int valor) {
+        verDescendentesNo(raiz, valor);
+    }
+
+    private boolean verDescendentesNo(Nodo nodo, int valor) {
+        if (nodo == null) {
+            return false;
+        }
+
+        if (nodo.key == valor) {
+            verDescendentes(nodo.left);
+            verDescendentes(nodo.right);
+            return true; 
+        }
+
+        if (verDescendentesNo(nodo.left, valor) || verDescendentesNo(nodo.right, valor)) {
+            return true;
+        }
+
+        return false;
+    }
+
+    private void verDescendentes(Nodo nodo) {
+        if (nodo == null) {
+            return;
+        }
+        
+        System.out.print(nodo.key + " ");
+        verDescendentes(nodo.left);
+        verDescendentes(nodo.right);
+    }
+	 
+	 
 }
+
+
